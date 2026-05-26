bd974c10cd6e82ec73635fcbb529277cad434c53
jlouange <j.mushumba@alustudent.com>
2026-05-26 05:39:15 -0700

    Shell script to check if a directory exists

---
+++ Dir_check.sh
@@ -0,0 +1,10 @@
+#!bin/sh
+
+Directory_name="assignment_dir"
+path="root/Shell_scripts/$Directory_name"
+
+if [ -d "$Path" ]; then
+	echo "Directory with a name'$Directory_name' exists in the Location"
+else
+	echo "No such a Directory"
+fi
